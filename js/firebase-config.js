49a094e177dd866e9477e0ecb28753afef5bde97
Leandro Coutinho <contatovortexcomunicao@gmail.com>
2026-02-11 21:02:42 -0300

    chore: versiona firebase-config para deploy online

---
+++ js/firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase Web Config
+// Este objeto não é um segredo; ele identifica seu projeto no Firebase.
+// Necessário para o app rodar no Firebase Hosting (online).
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBM9pCZEM7IKuiGfzV9Fw02wYE6RdRwvxE',
+  authDomain: 'vivahaven-2906a.firebaseapp.com',
+  projectId: 'vivahaven-2906a',
+  storageBucket: 'vivahaven-2906a.firebasestorage.app',
+  messagingSenderId: '76084318303',
+  appId: '1:76084318303:web:652655dd860ed151e28bea',
+};
